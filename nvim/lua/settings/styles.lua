d76fc6418566b287ed7f03c0ca1d6d6d2d297162
Jack Trusler <Jack.Trusler@TaaZaa.com>
2022-08-21 12:47:25 -0400

    (>^.^)>

--- nvim/lua/settings/styles.lua
+++ nvim/lua/settings/styles.lua
@@ -1,1 +1,6 @@
-vim.api.nvim_command('colorscheme gruvbox')
+local colorscheme = "gruvbox"
+local status_ok, _ = pcall(vim.api.nvim_command, "colorscheme " .. colorscheme)
+if not status_ok then
+    print("colorscheme " .. colorscheme .. " not found!")
+    return
+end
